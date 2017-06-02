f7e083655ac95e2ccaf433946abba8141a184428
khumbue <kncube68@yahoo.com>
2017-06-02 22:53:54 +0200

    Add files via upload

--- hydrological-analysis/hydrological-analysis-api/src/main/java/za/co/hydroanalysis/api/IHydrologicalAnalysisService.java
+++ hydrological-analysis/hydrological-analysis-api/src/main/java/za/co/hydroanalysis/api/IHydrologicalAnalysisService.java
@@ -1,61 +1,15 @@
-package za.co.jobcreation.api;
+package za.co.hydroanalysis.api;
 
-import za.co.jobcreation.api.dto.*;
+import za.co.hydroanalysis.api.dto.StationDto;
+import za.co.hydroanalysis.api.dto.*;
 
 import java.util.Date;
 import java.util.List;
 
 public interface IHydrologicalAnalysisService {
 
-    Response addUser(UserDto employeeDto);
+    List<StationDto> getAllStationsData();
 
-    Response editUser(UserDto employeeDto);
+    StationDto getStationData(String stationName);
 
-    Response deleteUser(UserDto employeeDto);
-
-    boolean isUserActive(String userName);
-
-    List<UserDto> getAllUsers();
-
-    List<EnterpriseDto> getAllEnterprises();
-
-    List<ProvinceDto> getAllProvinces();
-
-    List<IndustrySectorDto> getAllIndustrySectors();
-
-    Response addEnterprise(EnterpriseDto enterpriseDto);
-
-    Response editEnterprise(EnterpriseDto enterpriseDto);
-
-    List<RoleDto> getAllRoles();
-
-    UserDto getUserByUserName(String userName);
-
-    Response addJobs(JobsDto jobsDto);
-
-    Response editJobs(JobsDto jobsDto);
-
-    JobsDto getJobsByEnterpriseAndProvince(String enterprise, String province, Date date);
-
-    List<JobsDto> getAllJobs();
-
-    List<ReportCriteriaDto> getAllReportCriterion();
-
-    List<JobsDto> getJobsForAllProvincesForRange(Date fromDate, Date toDate);
-
-    List<JobsDto> getEnterpriseJobsForRange(String enterprise, Date fromDate, Date toDate);
-
-    List<DepartmentDto> getAllDepartments();
-
-    Response addDepartment(DepartmentDto departmentDto);
-
-    Response editDepartment(DepartmentDto departmentDto);
-
-    List<ChartTypeDto> getAllChartTypes();
-
-    List<JobsDto> getDepartmentJobsForRange(String department, Date fromDate, Date toDate);
-
-    List<AnnualTurnOverDto> getAllAnnualTurnOvers();
-
-    List<BbbeeRatingDto> getAllBbbeeRatings();
 }
