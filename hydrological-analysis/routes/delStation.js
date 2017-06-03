1c111296e88ed0f047047673a667f6a7057a2611
7Tranquility <7tranquility@gmail.com>
2017-06-03 16:26:07 +0200

    Add files via upload
    V1.1beta

---
+++ hydrological-analysis/routes/delStation.js
@@ -0,0 +1,51 @@
+ // Imports all the modules needed
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
+
+// Used to connect to the MongoDB database
+
+var router = express.Router();
+
+
+//Stations API
+router.get('/:col&:doc', function(req, res){
+//initialise col with request parm
+  var col = req.params.col;
+  var doc = req.params.doc;
+
+  // Get a Mongo client to work with the Mongo server
+  var MongoClient = mongodb.MongoClient;
+
+  // Define where the MongoDB server is
+  var url = 'mongodb://localhost:27017/HYDRA';
+
+  // Connect to the server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the Server', err);
+  } else {
+    // We are connected
+    console.log('Connection established to', url);
+
+    // Get the documents collection
+    var collection = db.collection(col);
+
+    // Find all students
+    collection.deleteOne({$or:[{"Station_No":doc}, {"Peak_Date":parseInt(doc)}]}).then(function (err, result) {
+      if (err) {
+        res.send(err);
+      } else if (result.length) {
+        res.json(result);
+      } else {
+        res.send('No documents found');
+      }
+      //Close connection
+      db.close();
+    });
+  }
+  });
+});
+
+
+module.exports = router;
